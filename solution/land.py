13496423726d3d2e890fe09d960ae775310809ea
Neel Kuila <neelk1123@gmail.com>
2021-03-21 13:26:32 -0700

    noted missing aez29 data, use constant

--- solution/land.py
+++ solution/land.py
@@ -8,6 +8,7 @@ MHA_TO_HA = 1000000
 # Gets the total land allocation per moisture regime.
 # Set per_aez to True to get land totals per each AEZ
 # Returns pd.Series
+# NOTE: AEZ 29 data is missing
 def get_land_totals(per_aez=False):
     # It doesn't matter which solution or scenario is used here.
     # Under the hood, each solution looks at the same set of files in data/land/world/2020/
@@ -19,7 +20,7 @@ def get_land_totals(per_aez=False):
     for mr in d.keys():
         for aez in d[mr].keys():
             tuples.append((mr, aez))
-            values.append(d[mr][aez]['TOTAL'] / 10000)
+            values.append(d[mr][aez]['TOTAL'] / MHA_TO_HA)
     index = pd.MultiIndex.from_tuples(tuples, names=['Moisture Regime', 'AEZ'])
     series = pd.Series(values, index=index)
 
